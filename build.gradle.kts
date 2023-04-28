6a585e13019ae4ccbf40fa2c34f2fe4a3c5e27d5
pRaNaY <www.pranay1337.com@gmail.com>
2023-04-28 11:22:23 +0530

    :rocket: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("multiplatform") apply false
+    kotlin("android") apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.compose") apply false
+    alias(libs.plugins.ktLint)
+    alias(libs.plugins.detekt)
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    ktlint {
+        debug.set(true)
+        verbose.set(true)
+        android.set(false)
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+        filter {
+            enableExperimentalRules.set(true)
+            exclude { projectDir.toURI().relativize(it.file.toURI()).path.contains("/generated/") }
+            include("**/kotlin/**")
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    detekt {
+        parallel = true
+        config = files("${project.rootDir}/config/detekt/detekt.yml")
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
