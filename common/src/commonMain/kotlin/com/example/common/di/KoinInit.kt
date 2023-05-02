7eb96f88e789515c5c1a7e59f6e5b0af5663528b
pRaNaY <www.pranay1337.com@gmail.com>
2023-05-02 18:30:13 +0530

    :art: Update network module and koin init

--- common/src/commonMain/kotlin/com/example/common/di/KoinInit.kt
+++ common/src/commonMain/kotlin/com/example/common/di/KoinInit.kt
@@ -3,9 +3,9 @@ package com.example.common.di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
-fun initKoin(koinAppDeclaration: KoinAppDeclaration={}){
+fun initKoin(koinAppDeclaration: KoinAppDeclaration = {}) {
     startKoin {
         koinAppDeclaration()
-        modules(networkModule)
+        modules(networkModule, repositoryModule, presenterModule)
     }
 }
--- common/src/commonMain/kotlin/com/example/common/di/NetworkModule.kt
+++ common/src/commonMain/kotlin/com/example/common/di/NetworkModule.kt
@@ -13,7 +13,6 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.URLProtocol
-import io.ktor.http.path
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
@@ -32,16 +31,16 @@ val networkModule = module {
                 }
             }
 
-                install(Logging) {
-                    level = LogLevel.HEADERS
-                    logger = object : Logger {
-                        override fun log(message: String) {
-                            Napier.e(tag = "Http Client", message = message)
-                        }
+            install(Logging) {
+                level = LogLevel.HEADERS
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Napier.e(tag = "Http Client", message = message)
                     }
-                }.also {
-                    Napier.base(DebugAntilog())
                 }
+            }.also {
+                Napier.base(DebugAntilog())
+            }
 
             install(ContentNegotiation) {
                 json(
